143631a731d0165907a3ee6293e9e0b6bd3449cd
SUMIT HIREMATH <123145236+hiremath4@users.noreply.github.com>
2025-07-05 12:21:52 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'db';
-$db = 'ogtech';
-$user = 'user';
-$pass = 'pass';
+$host = getenv('DB_HOST') ?: 'localhost';
+$db   = getenv('DB_NAME') ?: 'ogtech';
+$user = getenv('DB_USER') ?: 'user';
+$pass = getenv('DB_PASS') ?: 'pass';
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
